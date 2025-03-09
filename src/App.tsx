bfec3f700f8e1f1ffd529b8ac309798e9d5ff1ce
Webaldar <webaldar@gmail.com>
2025-03-09 14:49:20 +0300

    4 lesson complete

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import styles from "./components/Site.module.css";
+import {PageOne} from "./components/pages/PageOne";
+import {PageTwo} from "./components/pages/PageTwo";
+import {PageThree} from "./components/pages/PageThree";
+import {Navigate, NavLink, Route, Routes} from "react-router-dom";
+import {Error404} from "./components/pages/Error404";
+
+
+function App() {
+    return (
+        <div>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    <div><NavLink
+                        to={'/page1'}
+                        className={({isActive}) =>
+                            isActive ? styles.activeNavLink : styles.navLink}>Page1</NavLink></div>
+                    <div><NavLink
+                        to={'/page2'} className={({isActive}) =>
+                        isActive ? styles.activeNavLink : styles.navLink}>Page2</NavLink></div>
+                    <div><NavLink
+                        to={'/page3'} className={({isActive}) =>
+                        isActive ? styles.activeNavLink : styles.navLink}>Page3</NavLink></div>
+                </div>
+                <div className={styles.content}>
+                    <Routes>
+                        <Route path='/page1' element={<PageOne/>}/>
+                        <Route path='/' element={<Navigate to={'/page1'}/>}/>
+                        <Route path='/page2' element={<PageTwo/>}/>
+                        <Route path='/page3' element={<PageThree/>}/>
+                        <Route path='/error404' element={<Error404/>}/>
+
+                        <Route path='/*' element={<Navigate to={'/error404'}/>}/>
+                    </Routes>
+
+                </div>
+            </div>
+            <div className={styles.footer}>abibas 2023</div>
+        </div>
+    );
+}
+
+
+export default App;
